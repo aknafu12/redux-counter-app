21c0d3b2bf1f12c64d969d61f5f3029314fdffc1
aknafu12 <kgebre2021@gmail.com>
2024-03-27 10:05:02 +0300

    intial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Welcome to Redux-counter -App</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
